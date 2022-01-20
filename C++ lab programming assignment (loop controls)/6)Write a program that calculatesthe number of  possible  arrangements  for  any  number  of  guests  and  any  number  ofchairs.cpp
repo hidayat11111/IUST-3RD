5cb1b11777204a41e98ab44cc568f4b541b1e39b
Hidayat ali beigh <96649174+hidayat11111@users.noreply.github.com>
2022-01-20 22:45:41 +0530

    Add files via upload

---
+++ C++ lab programming assignment (loop controls)/6)Write a program that calculatesthe number of  possible  arrangements  for  any  number  of  guests  and  any  number  ofchairs.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+int main (){
+    int i,guest,chairs,total=1;
+    //taking input
+    cout<<"Enter the no. of guest";
+    cin>>guest;
+    //taking input
+    cout<<"Enter the no. of chairs";
+    cin>>chairs;
+    //conditional statement
+    if(guest>chairs){
+        for (int i = 0; i < chairs; i++) {
+            total=total*guest;
+            guest--; //decerement operator
+        }
+    }
+    //final result
+    cout<<"No. of possible arrangements are:"<<total;
+    return 0;}
